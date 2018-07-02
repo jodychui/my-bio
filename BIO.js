9b9f01681ce4c9aea5854afa356621e8ea7575f7
jodychui <34219764+jodychui@users.noreply.github.com>
2018-07-02 09:14:12 -0700

    Add files via upload

---
+++ BIO.js
@@ -0,0 +1,29 @@
+function hungryImg() {
+	var img = document.getElementById("hungry"); 
+	img.style.display = "block"; 
+}
+
+function happyImg() {
+	var img = document.getElementById("happy");
+	img.style.display = "block";
+}
+
+function tiredImg() {
+	var img = document.getElementById("tired");
+	img.style.display = "block";
+}
+
+
+function openSection(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
